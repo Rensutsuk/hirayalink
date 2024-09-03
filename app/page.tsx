055b381590cc3501d932c1f701b802d3c3de21aa
Rensutsu <johnrenzoerfelp@gmail.com>
2024-09-03 14:25:39 +0800

    style: Refactor landing page and fix import path in home page
    Refactor landing page to include hero section with background image, logo, and 
    call-to-action button. Also fixed the import path for 'embla.css' in the home 
    page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,25 @@
 export default function Landing() {
-  return(
-    <main>
-      <div>
-        
-      </div>
-    </main>
-  );
+	return (
+		<main>
+			<div
+				className="hero min-h-screen"
+				style={{
+					backgroundImage:
+						"url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
+				}}
+			>
+				<div className="hero-overlay backdrop-blur-sm"></div>
+				<div className="hero-content text-neutral-content text-center">
+					<div className="max-w-md">
+						<img src="./aidlink.svg" alt="HirayaLink" />
+						<h1 className="mb-5 text-5xl font-bold">
+							<span className="text-primary">HIRAYA</span>
+							<span>LINK</span>
+						</h1>
+						<a role="button" href="/home" className="btn btn-primary">Get Started</a>
+					</div>
+				</div>
+			</div>
+		</main>
+	);
 }
