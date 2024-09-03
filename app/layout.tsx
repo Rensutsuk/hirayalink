866099d0d7b24ed1556d59f75c1cf1198d14e5c5
Rensutsu <johnrenzoerfelp@gmail.com>
2024-09-03 22:21:50 +0800

    style: Update global CSS imports to use the styles directory
    - Move global CSS imports from 'globals.css' to 'styles/globals.css'
    - Ensure consistency and better organization of styles.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
-
-import Navbar from "@/components/navigation/Navbar";
-import Footer from "@/components/navigation/Footer";
+import "@/styles/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,9 +17,7 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body className={inter.className}>
-				<Navbar />
 				{children}
-				<Footer />
 			</body>
 		</html>
 	);
