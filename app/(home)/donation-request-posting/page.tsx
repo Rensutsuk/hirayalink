74ed153a7d427299c7db305159c8a39150be8948
RosieHernan04 <hernanroseann4@gmail.com>
2024-10-02 11:47:55 +0800

    donation-request-posting

--- app/(home)/donation-request-posting/page.tsx
+++ app/(home)/donation-request-posting/page.tsx
@@ -285,7 +285,7 @@ export default function DonationRequestPosting() {
                                     className="block text-gray-700 text-sm font-bold mb-2"
                                     htmlFor="proofFile"
                                 >
-                                    Proof of Residence (e.g., Government ID, Utility Bill)
+                                    Proof of Situation (e.g., Picture, Family Card)
                                 </label>
                                 <input
                                     className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
