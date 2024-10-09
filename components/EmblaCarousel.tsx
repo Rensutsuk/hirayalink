2d9d36708fc99f7145d4ec5c9013b01091b37902
RosieHernan04 <hernanroseann4@gmail.com>
2024-10-09 14:09:53 +0800

    stories-of-change

--- components/EmblaCarousel.tsx
+++ components/EmblaCarousel.tsx
@@ -1,56 +1,36 @@
-import React, { useEffect } from "react";
+import React from "react";
 import useEmblaCarousel from "embla-carousel-react";
 import Autoplay from "embla-carousel-autoplay";
 
-export function EmblaCarousel() {
-	const [emblaRef] = useEmblaCarousel({ loop: false }, [Autoplay()]);
+interface Slide {
+  src: string;
+  description: string;
+}
 
-	return (
-		<div className="embla" ref={emblaRef}>
-			<div className="embla__container">
-				<div className="embla__slide">
-					<div className="card bg-base-100 w-96 shadow-xl">
-						<figure>
-							<img
-								src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-								alt="Shoes"
-							/>
-						</figure>
-						<div className="card-body">
-							<h2 className="card-title">Shoes!</h2>
-							<p>If a dog chews shoes whose shoes does he choose?</p>
-						</div>
-					</div>
-				</div>
-				<div className="embla__slide">
-					<div className="card bg-base-100 w-96 shadow-xl">
-						<figure>
-							<img
-								src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-								alt="Shoes"
-							/>
-						</figure>
-						<div className="card-body">
-							<h2 className="card-title">Shoes!</h2>
-							<p>If a dog chews shoes whose shoes does he choose?</p>
-						</div>
-					</div>
-				</div>
-				<div className="embla__slide">
-					<div className="card bg-base-100 w-96 shadow-xl">
-						<figure>
-							<img
-								src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-								alt="Shoes"
-							/>
-						</figure>
-						<div className="card-body">
-							<h2 className="card-title">Shoes!</h2>
-							<p>If a dog chews shoes whose shoes does he choose?</p>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+interface EmblaCarouselProps {
+  slides: Slide[];
+}
+
+export function EmblaCarousel({ slides }: EmblaCarouselProps) {
+  const [emblaRef] = useEmblaCarousel({ loop: false }, [Autoplay()]);
+
+  return (
+    <div className="embla" ref={emblaRef}>
+      <div className="embla__container">
+        {slides.map((slide, index) => (
+          <div className="embla__slide" key={index}>
+            <div className="card bg-base-100 w-96 shadow-xl">
+              <figure>
+                <img src={slide.src} alt={`Slide ${index}`} />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">Slide {index + 1}</h2>
+                <p>{slide.description}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
