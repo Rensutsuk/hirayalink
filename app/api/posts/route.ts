e092384cd6b42856beb592724170472d42b4235c
Rensutsuk <johnrenzoerfelp@gmail.com>
2024-10-31 20:58:50 +0800

    feat: Add user id to select statement in GET function

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -47,6 +47,7 @@ export async function GET(request: Request) {
             include: {
               user: {
                 select: {
+                  id: true,
                   name: true,
                 },
               },
